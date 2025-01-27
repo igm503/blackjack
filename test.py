5cc1f60c2ff47cdefb8db34cd8dd85eb465d9935
igm503 <igm503@gmail.com>
2025-01-27 00:16:50 -0600

    added dealer rollout and stand ev tests

--- test.py
+++ test.py
@@ -1,85 +1,131 @@
 from collections import defaultdict
 
-from main import get_move, Move, dealer_rollout, get_hand_evs
-from deck import Hand
-from counter import NoneCounter, Counter
+from tqdm import tqdm
 
-GREEN = "\033[92m"
-RED = "\033[95m"
-BLUE = "\033[94m"
-YELLOW = "\033[93m"
-ENDC = "\033[0m"
+from main import dealer_rollout, get_hand_evs
+from deck import Deck, Hand
+from counter import NoneCounter
 
 
-def get_dealer_face_string():
-    string = "     "
-    for dealer_face in range(2, 12):
-        string += f"| {dealer_face} "
-    return string + "\n"
+def get_dealer_hand(dealer_face, deck):
+    deck.shuffle()
+    second_card = deck.deal_card()
+    if dealer_face == 11:
+        while second_card == 10:
+            second_card = deck.deal_card()  # dealer already peaked
+    elif dealer_face == 10:
+        while second_card == 11:
+            second_card = deck.deal_card()
+    dealer = Hand([dealer_face, second_card])
+    return dealer
 
 
-def get_move_string(hand, dealer_face, counter):
-    move = get_move(hand, dealer_face, counter)
-    if move == Move.STAND:
-        return f"| {YELLOW}S{ENDC} "
-    elif move == Move.HIT:
-        return f"| {RED}H{ENDC} "
-    elif move == Move.DOUBLE:
-        return f"| {BLUE}D{ENDC} "
-    elif move == Move.SPLIT:
-        return f"| {GREEN}P{ENDC} "
-    else:
-        return "| ?"
+def dealer_action(hand, deck):
+    while hand.must_hit:
+        hit_card = deck.deal_card()
+        hand.add(hit_card)
 
 
-def basic_strategy_hard():
-    counter = NoneCounter(1)
+def test_dealer_rollout(verbose=False):
+    num_samples = 10_000
+    abs_err = 0.02
 
-    print(get_dealer_face_string())
+    num_decks = 8
 
-    for hand_value in range(5, 22):
-        card1 = hand_value // 2
-        card2 = hand_value - card1
-        if hand_value == 21:
-            hand = Hand([card1, card2, 0])
-        else:
-            hand = Hand([card1, card2])
-        string = ""
+    deck = Deck(num_decks)
+
+    errors = defaultdict(dict)
+
+    print(f"testing dealer rollout with hit on soft 17 set to {Hand.hit_soft_17}")
+
+    for dealer_face in tqdm(range(2, 12)):
+        sample_outcomes = defaultdict(float)
+
+        for _ in range(num_samples):
+            dealer = get_dealer_hand(dealer_face, deck)
+            dealer_action(dealer, deck)
+            value = dealer.value if not dealer.is_bust else 0
+            sample_outcomes[value] += 1
+
+        for value, count in sample_outcomes.items():
+            sample_outcomes[value] = count / num_samples
+
+        estimated_outcomes = dealer_rollout(dealer_face, NoneCounter(8))
+        for value, prob in estimated_outcomes.items():
+            error = abs(sample_outcomes[value] - prob)
+            if error > abs_err:
+                print(f"{dealer_face}: {sample_outcomes}")
+                print(f"{dealer_face}: {estimated_outcomes}")
+                raise Exception
+            errors[dealer_face][value] = error
+
+    if verbose:
+        final_value_string = "  "
+        for dealer_face in [0, 17, 18, 19, 20, 21]:
+            final_value_string += f"|  {dealer_face} "
+        print(final_value_string)
+
         for dealer_face in range(2, 12):
-            string += get_move_string(hand, dealer_face, counter)
-        string += "\n" + "-" * 45
-        print(f"{hand_value:3.0f}: {string}")
+            string = f"{dealer_face}: "
+            for final_value in [0, 17, 18, 19, 20, 21]:
+                string += f"{errors[dealer_face].get(final_value, 0):.3f} "
+            print(string)
 
 
-def basic_strategy_soft():
-    counter = NoneCounter(1)
+def test_stand_evs(verbose=False):
+    num_samples = 100_000
+    abs_err = 0.03
 
-    print(get_dealer_face_string())
+    num_decks = 8
 
-    for hand_value in range(13, 22):
-        card1 = 11
-        card2 = hand_value - 11
-        if hand_value == 21:
-            hand = Hand([card1, card2, 0])
-        else:
-            hand = Hand([card1, card2])
-        string = ""
+    deck = Deck(num_decks)
+
+    errors = defaultdict(dict)
+
+    print(f"testing stand evs with hit on soft 17 set to {Hand.hit_soft_17}")
+
+    for dealer_face in tqdm(range(2, 12)):
+        sample_outcomes = defaultdict(float)
+
+        for _ in range(num_samples):
+            dealer = get_dealer_hand(dealer_face, deck)
+            dealer_action(dealer, deck)
+            value = dealer.value if not dealer.is_bust else 0
+            for player_value in range(4, 22):
+                if player_value > value:
+                    sample_outcomes[player_value] += 1
+                elif player_value < value:
+                    sample_outcomes[player_value] -= 1
+
+        for value, count in sample_outcomes.items():
+            sample_outcomes[value] = count / num_samples
+
+        dealer_probs = dealer_rollout(dealer_face, NoneCounter(8))
+        estimated_outcomes, _, _ = get_hand_evs(dealer_probs, NoneCounter(8))
+
+        for (hand_value, is_soft), prob in estimated_outcomes.items():
+            if is_soft:
+                assert estimated_outcomes[(hand_value, False)] == prob
+            error = abs(sample_outcomes[hand_value] - prob)
+            if error > abs_err:
+                print(f"{hand_value} {error}")
+                print(f"{dealer_face}: {sample_outcomes}")
+                print(f"{dealer_face}: {estimated_outcomes}")
+                raise Exception
+            errors[dealer_face][hand_value] = error
+
+    if verbose:
+        final_value_string = "  "
+        for hand_value in range(4, 22):
+            final_value_string += f"|  {hand_value} "
+        print(final_value_string)
+
         for dealer_face in range(2, 12):
-            string += get_move_string(hand, dealer_face, counter)
-        string += "\n" + "-" * 45
-        print(f"{hand_value:3.0f}: {string}")
+            string = f"{dealer_face}: "
+            for hand_value in range(4, 22):
+                string += f"{errors[dealer_face].get(hand_value, 0):.3f} "
+            print(string)
 
 
-basic_strategy_hard()
-print("\n\n")
-basic_strategy_soft()
-
-#
-# dealer_probs = dealer_rollout(11, NoneCounter(1))
-# stand_evs, hit_evs, double_evs = get_hand_evs(
-#     dealer_probs,
-#     NoneCounter(1),
-# )
-#
-# for i in sorted(hit_evs.keys()):
-#     print(f"{i}: {double_evs[i]:.3f} {hit_evs[i]:.3f} {stand_evs[i]:.3f}")
+test_stand_evs(verbose=True)
+test_dealer_rollout(verbose=True)
