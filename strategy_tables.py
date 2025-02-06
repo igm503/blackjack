3bd3a2e4d39555c597fc7f309b42991dbb388b00
igm503 <igm503@gmail.com>
2025-02-06 02:06:17 -0600

    strategy table gen uses card counting

--- strategy_tables.py
+++ strategy_tables.py
@@ -1,12 +1,14 @@
 from main import get_move, Move, should_surrender
-from deck import Hand
-from counter import NoneCounter
+from models.deck import Hand
+from models.counter import PerfectCounter
 
 GREEN = "\033[91m"
 RED = "\033[95m"
 BLUE = "\033[94m"
 YELLOW = "\033[93m"
 ENDC = "\033[0m"
+
+NUM_DECKS = 8
 
 
 def get_dealer_face_string():
@@ -29,6 +31,7 @@ def get_move_string(hand, dealer_face, counter, num_splits=0):
     else:
         return "| ?"
 
+
 def get_surrender_string(hand, dealer_face, counter, num_splits=3):
     surrender = should_surrender(hand, dealer_face, counter, num_splits)
     if surrender:
@@ -38,62 +41,88 @@ def get_surrender_string(hand, dealer_face, counter, num_splits=3):
 
 
 def basic_strategy_hard():
-    counter = NoneCounter(1)
+    counter = PerfectCounter(NUM_DECKS)
 
     print(get_dealer_face_string())
 
     for hand_value in range(5, 22):
         card1 = min(hand_value - 2, 10)
         card2 = hand_value - card1
+        counter.count(card1)
+        counter.count(card2)
         hand = Hand([card1, card2])
         string = ""
         for dealer_face in range(2, 12):
+            counter.count(dealer_face)
             string += get_move_string(hand, dealer_face, counter)
+            counter.uncount(dealer_face)
+        counter.uncount(card1)
+        counter.uncount(card2)
         string += "\n" + "-" * 45
         print(f"{hand_value:3.0f}: {string}")
 
 
 def basic_strategy_soft():
-    counter = NoneCounter(1)
+    counter = PerfectCounter(NUM_DECKS)
 
     print(get_dealer_face_string())
 
     for hand_value in range(13, 22):
         card1 = 11
         card2 = hand_value - 11
+        counter.count(card1)
+        counter.count(card2)
         hand = Hand([card1, card2])
         string = ""
         for dealer_face in range(2, 12):
+            counter.count(dealer_face)
             string += get_move_string(hand, dealer_face, counter)
+            counter.uncount(dealer_face)
+        counter.uncount(card1)
+        counter.uncount(card2)
         string += "\n" + "-" * 45
         print(f"{hand_value:3.0f}: {string}")
 
 
 def basic_strategy_pair():
-    counter = NoneCounter(1)
+    counter = PerfectCounter(NUM_DECKS)
 
     print(get_dealer_face_string())
 
     for card in range(2, 12):
+        counter.count(card)
+        counter.count(card)
         hand = Hand([card, card])
         string = ""
         for dealer_face in range(2, 12):
+            counter.count(dealer_face)
             string += get_move_string(hand, dealer_face, counter, num_splits=3)
+            counter.uncount(dealer_face)
         string += "\n" + "-" * 45
         print(f"{card}: {string}")
+        counter.uncount(card)
+        counter.uncount(card)
+
 
 def surrender_strategy():
-    counter = NoneCounter(1)
+    counter = PerfectCounter(NUM_DECKS)
     print(get_dealer_face_string())
     for hand_value in range(5, 22):
         card1 = min(hand_value - 2, 10)
         card2 = hand_value - card1
+        counter.count(card1)
+        counter.count(card2)
         hand = Hand([card1, card2])
         string = ""
         for dealer_face in range(2, 12):
+            counter.count(dealer_face)
             string += get_surrender_string(hand, dealer_face, counter, num_splits=3)
+            counter.uncount(dealer_face)
+        counter.uncount(card1)
+        counter.uncount(card2)
         string += "\n" + "-" * 45
         print(f"{hand_value:3.0f}: {string}")
+
 
 basic_strategy_hard()
 print("\n\n")
